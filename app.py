dc106ec204eca7466d218bfcbf80440983fb591b
StealthyFearZ <74085020+StealthyFearZ@users.noreply.github.com>
2023-06-03 18:54:59 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+# importing modules from flask library
+from flask import Flask , render_template
+import static
+# creating instance of class Flask, by providing __name__ keyword as argument
+app = Flask(__name__)
+
+# write the routes using decorator functions
+# default route or 'URL'
+@app.route("/")
+def home():
+
+    name = "Daksh Raghuvanshi" # write your name
+    age = "17" # write your age
+
+    return render_template('index.html' , name = name , age = age)
+
+# define the route to father webpage
+@app.route("/father")
+def father():
+
+    name = "Rajesh Singh" # write your name
+    age = "53" # write your age
+
+    return render_template('father.html' , name = name , age = age)
+# define the route to mother webpage
+@app.route("/mother")
+def mother():
+
+    name = "Meenakshi Singh" # write your name
+    age = "48" # write your age
+
+    return render_template('mother.html' , name = name , age = age)
+
+# define the route to friends webpage
+@app.route("/friend")
+def friend():
+
+    name = "Balbh East" # write your name
+    age = "17" # write your age
+
+    return render_template('friend.html' , name = name , age = age)
+
+# add other routes, if you want
+
+
+# run the file
+if __name__  ==  '__main__':
+    app.run(debug=True)
